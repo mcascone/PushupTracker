5ebd9f2333dfe7d64c2ec7558bb68feb8ae29eca
mcascone <max@bitovi.com>
2026-04-23 15:38:20 -0500

    Initial Commit

---
+++ PushupTrackerTests/PushupTrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  PushupTrackerTests.swift
+//  PushupTrackerTests
+//
+//  Created by Maximilian Cascone on 4/23/26.
+//
+
+import Testing
+@testable import PushupTracker
+
+struct PushupTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
