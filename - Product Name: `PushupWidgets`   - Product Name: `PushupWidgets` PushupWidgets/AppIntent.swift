4a7d3e959f0d8cfc229677ad529e99eca6db2d1e
mcascone <max@bitovi.com>
2026-04-24 12:46:32 -0500

    fix mangled names

---
+++ - Product Name: `PushupWidgets`   - Product Name: `PushupWidgets` PushupWidgets/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  - Product Name: `PushupWidgets`   - Product Name: `PushupWidgets` PushupWidgets
+//
+//  Created by Maximilian Cascone on 4/23/26.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ - Product Name: `PushupWidgets`   - Product Name: `PushupWidgets` PushupWidgets/__Product_Name___PushupWidgets______Product_Name___PushupWidgets__PushupWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  __Product_Name___PushupWidgets______Product_Name___PushupWidgets__PushupWidgetsBundle.swift
+//  - Product Name: `PushupWidgets`   - Product Name: `PushupWidgets` PushupWidgets
+//
+//  Created by Maximilian Cascone on 4/23/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct __Product_Name___PushupWidgets______Product_Name___PushupWidgets__PushupWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        __Product_Name___PushupWidgets______Product_Name___PushupWidgets__PushupWidgets()
+    }
+}
